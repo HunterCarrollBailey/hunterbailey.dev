6ef2e3fb1b441bf9057bbb6c4f0bf79f26d2f35a
Hunter Bailey <bailey@paradoxinnovators.com>
2024-07-28 18:49:40 -0700

    removed image from about background

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -3,7 +3,7 @@ import AboutBio from "../components/AboutBio";
 import Degree from "../components/Degree";
 export default function About() {
   return (
-    <main className="flex-grow overflow-scroll no-scrollbar h-96 bg-skills bg-cover bg-no-repeat">
+    <main className="flex-grow overflow-scroll no-scrollbar h-96 bg-cover bg-no-repeat">
       <section className="flex flex-col lg:flex-row w-full backdrop-blur bg-slate-200 lg:bg-slate-50/40 h-full">
         <div className="w-full p-10 flex flex-col lg:flex-row">
           <AboutBio />
